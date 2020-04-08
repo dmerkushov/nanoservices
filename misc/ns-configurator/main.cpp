e2e034f868e2c17a275e433db97b735d687cb71c
a-petelin <petelin.alexey@gmail.com>
2020-04-08 12:30:52 +0300

    Fix review suggesion about ns-configurator  On branch ag-develop 	modified:   misc/ns-configurator/main.cpp

--- misc/ns-configurator/main.cpp
+++ misc/ns-configurator/main.cpp
@@ -7,6 +7,18 @@
 using namespace std;
 using namespace nanoservices;
 
+NsSkelJsonPtr getJsonFromCmdArgs() {
+	vector<string> json_parts = NsCmdLineParameters::instance()->paramValues("data");
+	
+	string json = std::accumulate(std::next(json_parts.begin()), json_parts.end(),
+                            json_parts[0], // start with first element
+                            [](string a, string b) {
+			         return std::move(a) + ' ' + std::move(b);
+			     });
+
+	return NsSkelJsonParser().parse(json);
+}
+
 int main(int argc, char** argv) {
 	map<char, NsCmdLineParameters::opt> prms = {
 		{'p' , {"path", 'p', "PATH", 0, "Path to config node", 2}},
@@ -18,28 +30,35 @@ int main(int argc, char** argv) {
 		{'\0', {0,0,0,0}}
 	};
 	NsCmdLineParameters::init(prms, argc, argv);
-	AbstractConfig::init(URI);
-	if(NsCmdLineParameters::instance()->isParam("create")) {
-		string path = NsCmdLineParameters::instance()->paramValue("path");
-		vector<string> json_parts = NsCmdLineParameters::instance()->paramValues("data");
-		
-		string json = std::accumulate(std::next(json_parts.begin()), json_parts.end(),
-                                    json_parts[0], // start with first element
-                                    [](string a, string b) {
-								         return std::move(a) + ' ' + std::move(b);
-								     });
 
-		NsSkelJsonPtr obj = NsSkelJsonParser().parse(json);
-		AbstractConfig::instance()->create(path, obj);
-	} else if(NsCmdLineParameters::instance()->isParam("read")) {
+	try {
+		AbstractConfig::init(URI);
+		if(!NsCmdLineParameters::instance()->isParam("path")) {
+			cerr << "Parameter --path is required." << endl;
+			return EXIT_FAILURE;
+		}
 		string path = NsCmdLineParameters::instance()->paramValue("path");
-
-		cout << AbstractConfig::instance()->read(path)->serialize() << endl;
-	} else if(NsCmdLineParameters::instance()->isParam("delete")) {
-		string path = NsCmdLineParameters::instance()->paramValue("path");
-
-		AbstractConfig::instance()->del(path);
+		if(NsCmdLineParameters::instance()->isParam("create")) {
+			if(!NsCmdLineParameters::instance()->isParam("data")) {
+				cerr << "Parameter --data is required." << endl;
+				return EXIT_FAILURE;
+			}
+			AbstractConfig::instance()->create(path, getJsonFromCmdArgs());
+		} else if(NsCmdLineParameters::instance()->isParam("read")) {
+			cout << AbstractConfig::instance()->read(path)->serialize() << endl;
+		} else if(NsCmdLineParameters::instance()->isParam("delete")) {
+			AbstractConfig::instance()->del(path);
+		} else if(NsCmdLineParameters::instance()->isParam("update")) {
+			if(!NsCmdLineParameters::instance()->isParam("data")) {
+				cerr << "Parameter --data is required." << endl;
+				return EXIT_FAILURE;
+			}
+			AbstractConfig::instance()->update(path, getJsonFromCmdArgs());
+		}
+		AbstractConfig::close();
+	} catch(exception& e) {
+		cerr << e.what() << endl;
+		return EXIT_FAILURE;
 	}
-	AbstractConfig::close();
 	return 0;
 }
