f51df0cdb30727b4db82e58b3d72adb420491341
a-petelin <petelin.alexey@gmail.com>
2020-04-08 07:31:20 +0300

    Fix english in misc/ns-shutdown/main.cpp
    Co-Authored-By: Dmitriy Merkushov <dmerkushov@users.noreply.github.com>

--- misc/ns-shutdown/main.cpp
+++ misc/ns-shutdown/main.cpp
@@ -10,7 +10,7 @@ using namespace nanoservices;
 
 int main(int argc, char** argv) {
 	map<char, NsCmdLineParameters::opt> prms = {
-		{'n' , {"name", 'n', "NAME", 0, "Name for shutdowning service"}},
+		{'n' , {"name", 'n', "NAME", 0, "Name of service to shutdown"}},
 		{'p' , {"port", 'p', "PORT", 0, "Port of shutdowning service"}},
 		{'h' , {"host", 'h', "HOST", 0, "Host of shutdowning service"}},
 		{'\0', {0,0,0,0}}
